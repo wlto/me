17d103205ed878567318c2a3deef0f2574c67e5b
wlto <to_william@icloud.com>
2022-04-10 00:32:11 -0400

    feat: started on easter egg feature

---
+++ src/components/Button.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let onClick;
+</script>
+
+<button class="button" on:click={onClick}>
+  <slot />
+</button>
+
+<style>
+  button {
+		font-family: "IBM Plex Mono", monospace;
+		font-size: 1rem;
+		font-weight: 400;
+		text-align: center;
+		color: var(--colour-black);
+    background: none;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+  }
+</style>
